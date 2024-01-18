c547929d1535e4f505d542af02a3c2e708c9819a
bunanbasu <156767488+bunanbasu@users.noreply.github.com>
2024-01-18 13:49:44 +0530

    Update models.py
    Added open AI document embedding

--- training-module/models.py
+++ training-module/models.py
@@ -1,11 +1,12 @@
 from transformers import BertModel, BertTokenizer
 import torch
+import openai
 
 #All the vector embedding Models
 
 #Model Name pre-trained BERT Model
 #Target file type PDF
-def generate_document_vector_bert (text):
+def generate_document_vector_bert (text, config):
     # Load pre-trained BERT model and tokenizer
     model_name = 'bert-base-uncased'
     tokenizer = BertTokenizer.from_pretrained(model_name)
@@ -15,3 +16,19 @@ def generate_document_vector_bert (text):
         output = model(**tokens)
     embeddings = output.last_hidden_state.mean(dim=1).squeeze().numpy()
     return embeddings
+
+
+def generate_document_vector_openai (text, config):
+    openai.api_key = config ['openai.api_key']
+    response = openai.Completion.create(
+        engine="text-embedding-ada-002",
+        prompt=text,
+        temperature=0,
+        max_tokens=768,
+        n=1,
+    )
+    embeddings = response['choices'][0]['text']
+    return embeddings
+
+
+     
